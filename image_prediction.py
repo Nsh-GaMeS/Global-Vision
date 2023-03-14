ae8b6569aeb41abbfab0be7391422a12a2d45055
Nsh-GaMeS <sunnatan6@gmail.com>
2023-03-13 21:51:18 -0400

    add first working code

--- image_prediction.py
+++ image_prediction.py
@@ -14,12 +14,15 @@ prediction.setModelTypeAsResNet50()
 prediction.setModelPath(os.path.join(execution_path, "resnet50-19c8e357.pth")) # Download the model via this link https://github.com/OlafenwaMoses/ImageAI/releases/download/3.0.0-pretrained/resnet50-19c8e357.pth
 prediction.loadModel()
 
-filePath = "{execution_path}\{dir}\{filename}".format(execution_path=execution_path, dir = "images",filename = args.fileName)
-predictions, probabilities = prediction.classifyImage(os.path.join(execution_path, filePath), result_count=10)
+img_dir = args.fileName 
+print(img_dir)
+#filePath = "{execution_path}\{dir}\{filename}".format(execution_path=execution_path, dir = "images",filename = args.fileName)
+# predictions, probabilities = prediction.classifyImage(os.path.join(execution_path, filePath), result_count=10)
+predictions, probabilities = prediction.classifyImage(img_dir, result_count=10)
 
 for eachPrediction, eachProbability in zip(predictions, probabilities):
     print(eachPrediction , " : " , eachProbability)
-print (predictions[0])
+print ("\n"+ predictions[0])
 
 mytext = predictions[0]
   
@@ -27,6 +30,6 @@ language = 'en'
 
 myobj = gTTS(text=mytext, lang=language, slow=False)
    
-myobj.save("welcome.mp3")
+myobj.save("prediction.mp3")
 
-os.system("welcome.mp3")
+os.system("prediction.mp3")
--- main.py
+++ main.py
@@ -1,36 +1,49 @@
 import cv2
 import os
 
-directory = "D:\Comp-Eng-2023\ImageAI\ourCode\images"
-cam = cv2.VideoCapture(0)
+img_dir = "D:\Comp-Eng-2023\ImageAI\ourCode\images"
 
-cv2.namedWindow("test")
 
-img_counter = 0
+def get_photo(): # method to take a photo and save it to "images" folder
+    img_counter = 0
+    directory = "D:\Comp-Eng-2023\ImageAI\ourCode\images"
+    cam = cv2.VideoCapture(0)
+    
+    cv2.namedWindow("test")
 
-while True:
-    ret, frame = cam.read()
-    if not ret:
-        print("failed to grab frame")
-        break
-    cv2.imshow("test", frame)
+    while True:
+        ret, frame = cam.read()
+        if not ret:
+            print("failed to grab frame")
+            break
+        cv2.imshow("test", frame)
 
-    k = cv2.waitKey(1)
-    if k%256 == 27:
-        # ESC pressed
-        print("Escape hit, closing...")
-        break
+        k = cv2.waitKey(1)
+        if k%256 == 27:
+            # ESC pressed
+            print("Escape hit, closing...")
+            break
 
-    elif k%256 == 32:
-        os.chdir(directory)
-        # SPACE pressed
-        img_name = "opencv_frame_{}.png".format(img_counter)
-        cv2.imwrite(img_name, frame)
-        print("{} written!".format(img_name))
-        img_counter += 1
+        elif k%256 == 32:
+            os.chdir(directory)
+            # SPACE pressed
+            img_name = "frame_{}.png".format(img_counter)
+            cv2.imwrite(img_name, frame)
+            print("{} written!".format(img_name))
+            img_counter += 1
 
-cam.release()
+    cam.release()
+    cv2.destroyAllWindows()
+    
+def newest(path): # gets the latest created file
+    files = os.listdir(path)
+    paths = [os.path.join(path, basename) for basename in files]
+    return max(paths, key=os.path.getctime)
 
-cv2.destroyAllWindows()
-# os.system('python D:\Comp-Eng-2023\ImageAI\ImageAI\examples\image_prediction.py -f \car1.jpg')
-print("hello")
+def predict(dir):
+    os.chdir("..")
+    os.system('python .\image_prediction.py -f '+ dir)
+
+get_photo()
+dir_newest = newest(path = img_dir)
+predict(dir = dir_newest)
