b550141b77db364b0995d6534d5d98dd63713d79
Nsh-GaMeS <112564985+Nsh-GaMeS@users.noreply.github.com>
2023-04-03 12:55:54 -0400

    updated image prediction class
    Signed-off-by: Nsh-GaMeS <112564985+Nsh-GaMeS@users.noreply.github.com>

--- image_prediction.py
+++ image_prediction.py
@@ -3,33 +3,36 @@ import os
 import argparse
 from gtts import gTTS
 
+#a pretrained model for object recognition and detection is used here but it was slightly modified  
+# Download the model via this link https://github.com/OlafenwaMoses/ImageAI/releases/download/3.0.0-pretrained/resnet50-19c8e357.pth
+
+# create dynamic arg value (variables for when the function is called)
 parser = argparse.ArgumentParser()
 parser.add_argument("-f", "--fileName", type=str)
 
-args = parser.parse_args()
+args = parser.parse_args()#get and set the arguments from when th function was called
 execution_path = os.getcwd()
+img_dir = args.fileName 
 
+#inizialize the prediction model
 prediction = ImageClassification()
 prediction.setModelTypeAsResNet50()
-prediction.setModelPath(os.path.join(execution_path, "resnet50-19c8e357.pth")) # Download the model via this link https://github.com/OlafenwaMoses/ImageAI/releases/download/3.0.0-pretrained/resnet50-19c8e357.pth
+prediction.setModelPath(os.path.join(execution_path, "resnet50-19c8e357.pth")) 
 prediction.loadModel()
 
-img_dir = args.fileName 
-print(img_dir)
-#filePath = "{execution_path}\{dir}\{filename}".format(execution_path=execution_path, dir = "images",filename = args.fileName)
-# predictions, probabilities = prediction.classifyImage(os.path.join(execution_path, filePath), result_count=10)
+#process the image 
 predictions, probabilities = prediction.classifyImage(img_dir, result_count=10)
 
+#print the first prediction with the probability 
 for eachPrediction, eachProbability in zip(predictions, probabilities):
     print(eachPrediction , " : " , eachProbability)
 print ("\n"+ predictions[0])
 
-mytext = predictions[0]
-  
-language = 'en'
+def text_to_specch(text):
+    mytext = text
+    language = 'en'
+    myobj = gTTS(text=mytext, lang=language, slow=False)
+    myobj.save("prediction.mp3")
+    os.system("prediction.mp3")
 
-myobj = gTTS(text=mytext, lang=language, slow=False)
-   
-myobj.save("prediction.mp3")
-
-os.system("prediction.mp3")
+text_to_specch(predictions[0])
